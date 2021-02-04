11b6aca14b599d75957ef0fc1e4968e49d01dc83
Md. Sohan <47356541+sohan160115@users.noreply.github.com>
2021-02-04 11:42:07 +0600

    basic array method

--- Program.cs
+++ Program.cs
@@ -11,21 +11,49 @@ namespace Practice_C_Sharp
         {
 
 
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var numbers = new[] {0, 1, 2, 6, 4,5};
 
-            Console.WriteLine(numbers[0]);
+            Console.WriteLine("Length: "+numbers.Length);
 
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
 
-            var flags = new bool[3];
+           var index= Array.IndexOf(numbers, 4);
+           Console.WriteLine("Index of 9 is: "+ index);
 
-            flags[0] = true;
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+           Array.Clear(numbers,0,2);
 
+           foreach (var i in numbers)
+           {
+               Console.WriteLine("After clear:  "+ i);
+           }
+
+           var n = new int[3];
+
+           Array.Copy(numbers,n,3);
+
+           Console.WriteLine("After Copy into new array");
+
+           foreach (var i in n)
+           {
+               Console.WriteLine(i);
+           }
+
+
+
+           Array.Sort(numbers);
+
+           Console.WriteLine("After sorting: ");
+
+           foreach (var i in numbers)
+           {
+               Console.WriteLine(i);
+           }
+
+           Console.WriteLine("Effect of reverse: ");
+            Array.Reverse(numbers);
+           foreach (var i in numbers)
+           {
+               Console.WriteLine(i);
+           }
         }
     }
 }
